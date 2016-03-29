72833538e0ff53423f995d22c29a14e9f5715eff
Piet Brauer <piet@nerdishbynature.com>
2016-03-29 12:26:02 +0800

    Update dependencies

---
+++ TrashCanKitTests/TrashCanKitTestURLSession.swift
@@ -0,0 +1,9 @@
+//
+//  TrashCanKitTestURLSession.swift
+//  TrashCanKit
+//
+//  Created by Piet Brauer on 29/03/16.
+//  Copyright © 2016 nerdishbynature. All rights reserved.
+//
+
+import Foundation
